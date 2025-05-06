dee9cf3d15852eb7b25b70b09e1e8e80d181411e
Svetlana-Gubina <gubinas83@gmail.com>
2025-05-06 17:38:25 +0300

    Add theme toggler

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ menuButton.addEventListener('click', () => {
     menuButton.setAttribute('aria-expanded', 'false');
   }
 });
+
+// Dark / light mode
+const modeButton = document.querySelector('#mode');
+
+function changeMode() {
+  var element = document.body;
+  element.classList.toggle('light-mode');
+
+  if (element.classList.contains('light-mode')) {
+    localStorage.setItem('mode', 'light');
+  } else {
+    localStorage.setItem('mode', 'dark');
+  }
+}
+
+modeButton.addEventListener('click', changeMode);
+
+// Check localStorage on page load
+window.onload = function () {
+  var mode = localStorage.getItem('mode');
+  if (mode === 'light') {
+    document.body.classList.add('light-mode');
+  }
+};
